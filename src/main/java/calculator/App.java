bd9a2511197b0d92c6eb14efd5b0326c65549e5e
Robbie <robbie331@>
2024-04-29 20:23:48 +0900

    level 1 - 3

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -6,6 +6,7 @@ public class App {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
+
         System.out.print("첫 번째 숫자를 입력하세요: ");
         // Scanner를 사용하여 양의 정수를 입력받고 적합한 타입의 변수에 저장합니다.
         int num1 = sc.nextInt();
@@ -18,14 +19,28 @@ public class App {
         char operator = sc.next().charAt(0);
 
         int result = 0;
-        // 연산자를 switch case 문으로 나누고 이 연산자들이 아닌 다른 걸 입력하면 default 로 0출력
-            result = switch(operator) {
-                case '+' -> num1 + num2;
-                case '-' -> num1 - num2;
-                case '*' -> num1 * num2;
-                case '/' -> num1 / num2;
-                default -> 0;
-            };
+        // 연산자를 switch case 문으로 나누고 이 연산자들이 아닌 다른 걸 입력하면 default 로 오류메시지와 함께 0 출력
+        switch (operator) {
+            case '+':
+                result = num1 + num2;
+                break;
+            case '-':
+                result = num1 - num2;
+                break;
+            case '*':
+                result = num1 * num2;
+                break;
+            case '/':
+                // num2는 0이 될 수 없다는 조건 추가
+                if (num2 != 0) {
+                    result = num1 / num2;
+                } else {
+                    System.out.println("두번째 숫자는 0이 될 수 없습니다.");
+                }
+                break;
+            default:
+                System.out.println("잘못된 연산자입니다.");
+        }
         System.out.println("결과: " + result);
     }
 }
