6831e0753e4911f987031890564495094fa7d988
Robbie <robbie331@>
2024-04-30 11:51:58 +0900

    level 1 - 7

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -1,12 +1,13 @@
 package calculator;
 
+import java.util.ArrayList;
 import java.util.Objects;
 import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int[] results = new int[10]; // 크기가 10인 정수 배열을 new 키워드로 생성
+        ArrayList<Integer> results = new ArrayList<>(); // ArrayList로 연산결과 저장
         int count = 0; // 연산 결과를 저장할 때 변수 초기화(저장된 연산결과 개수 추적) index=count
 
         // 무한루프를 위해 while (true)로 항상 참일 때 연산
@@ -44,25 +45,26 @@ public class App {
                     System.out.println("올바른 연산자를 입력하세요.");
                     continue; // continue 로 다음 반복으로 이동
             }
+            results.add(result); // 연산 결과 리스트에 추가
+
             System.out.println("결과: " + result);
 
-            /* 연산 결과가 10개를 초과하는 경우 */
-            if (count == results.length) {
-                // 가장 먼저 저장된 결과 삭제
-                for (int i=0; i<results.length -1; i++) {
-                    results[i] = results[i+1]; // 앞으로 한칸씩 이동
-                } results[count - 1] = result; // 새로운 결괏값 마지막 인덱스에 저장
-            } else {
-                results[count] = result;
-                count++; // index = count 증가
-            }
-
-            System.out.print("더 계산하시겠습니까? (exit 입력 시 종료): ");
+            System.out.print("가장 먼저 저장된 연산 결과를 삭제하시겠습니까? (remove 입력 시 종료): ");
             String input = sc.next();
-            // 두 객체를 비교하는 Objects.equals로 input(String타입)이 "exit"이면 sout띄우고 break로 무한루프 나오기
-            if (Objects.equals(input,"exit")) {
-                System.out.println("프로그램을 종료합니다.");
-                break; // 무한 루프 종료
+            /* 두 객체를 비교하는 Objects.equals로 input(String타입)이 "remove"이면 결괏값이 중첩 if문으로 비어있는지 확인 */
+            if (Objects.equals(input,"remove")) {
+                if (results.isEmpty()) {
+                    System.out.println("삭제할 결과가 없습니다.");
+                } else {
+                    int removedResult = results.remove(0); // 가장 먼저 저장된 결과 삭제
+                    System.out.println("removedResult = " + removedResult);
+                }
+                System.out.println("더 계산하시겠습니까? (exit 입력 시 종료): ");
+                input = sc.next();
+                if (Objects.equals(input,"exit")) {
+                    System.out.println("프로그램을 종료합니다.");
+                    break; // 무한 루프 종료
+                }
             }
         }
         sc.close(); // 스캐너 닫기
